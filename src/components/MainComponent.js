f6b767e0f8c3b3dcbdcb2cf49b0d0caaeaacf623
= <aryamanaranya@gmail.com>
2021-05-30 20:30:34 +0530

    Building and Deploying the Application

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -33,9 +33,6 @@ const mapDispatchtoProps = (dispatch) => ({
 });
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-  }
 
   componentDidMount() {
     this.props.fetchDishes();
